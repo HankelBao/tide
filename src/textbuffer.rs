32fbc36fd03eb7c633c2f0acf386c7123896b520
HankelBao <hankelbao@outlook.com>
2019-01-31 23:42:32 +0800

    switch back

--- src/textbuffer.rs
+++ src/textbuffer.rs
@@ -45,61 +45,23 @@ impl CursorPos {
     }
 }
 
-struct LineInfo {
-    buffer_index: usize,
-    buffer_offset: usize,
-    length: usize,
-    rendered_string: Option<String>,
-}
-
-impl LineInfo {
-    fn new() -> LineInfo {
-        return LineInfo {
-            buffer_index: 0,
-            buffer_offset: 0,
-            length: 0,
-            rendered_string: None,
-        }
-    }
-}
-
 pub struct TextBuffer {
     cursor_pos: CursorPos,
-    buffers: Vec<Box<String>>,
-    line_info: Vec<Box<LineInfo>>,
+    lines: Vec<Box<String>>,
+    rendered_cached: Vec<Option<String>>,
 }
 
 impl TextBuffer {
     pub fn new() -> TextBuffer {
-        let mut textbuffer = TextBuffer {
+        let textbuffer = TextBuffer {
             cursor_pos: CursorPos::from(0, 0),
-            buffers: vec![Box::new(String::from(""))],
-            line_info: vec![Box::new(LineInfo::new())],
+            lines: vec![Box::new(String::new())],
+            rendered_cached: vec![None],
         };
         return textbuffer;
     }
 
     pub fn insert_at(&mut self, line_offset: u32, line_num: u32, ch: char) {
-        let buffer_index = self.line_info[line_num as usize].buffer_index;
-        let buffer_offset = self.line_info[line_num as usize].buffer_offset;
-        if buffer_offset + line_offset as usize == self.buffers[buffer_index].len() {
-            self.buffers[buffer_index].push(ch);
-        } else {
-            let rest_str = self.buffers[buffer_index].split_off(buffer_offset + line_offset as usize);
-            self.buffers.insert(buffer_index+1, Box::new(String::from(rest_str)));
-            self.buffers[buffer_index].push(ch);
-        }
-    }
-
-    pub fn backspace_at(&mut self, line_offset: u32, line_num: u32) {
-        let buffer_index = self.line_info[line_num as usize].buffer_index;
-        let buffer_offset = self.line_info[line_num as usize].buffer_offset;
-    }
-
-    pub fn debug_print(&self) {
-        for buffer in &self.buffers {
-            print!("{}: ", buffer.capacity());
-            println!("{}", buffer);
-        }
+        self.lines[line_offset as usize].insert(line_offset as usize + 1, ch);
     }
 }
